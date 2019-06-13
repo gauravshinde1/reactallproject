a1b6a697b25205613fa5c32f76fd11e540c066ce
gaurav shinde <shinde786gaurav@gmail.com>
2019-06-13 18:57:04 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import './assets/css/Projectone.css'
-import Main from './Components/Main';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Main/>
-    </React.Fragment>
-  );
+import ProjectOneMain from './Components/ProjectOne/ProjectOneMain';
+import ProjectTwoMain from './Components/ProjectTwo/ProjectTwoMain';
+import ProjectThreeMain from './Components/ProjectThree/ProjectThreeMain';
+import ProjectFourMain from './Components/ProjectFour/ProjectFourMain';
+import Header from './Header';
+
+export class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Header/>
+                <Router>
+                    <Route exact path="/projectone" component={ProjectOneMain}/>
+                    <Route exact path="/projectTwo" component={ProjectTwoMain}/>
+                    <Route exact path="/projectThree" component={ProjectThreeMain}/>
+                    <Route exact path="/projectFour" component={ProjectFourMain}/>
+                </Router>
+            </React.Fragment>
+        )
+    }
 }
-
 export default App;
