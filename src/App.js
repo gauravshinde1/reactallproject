7f4366b416fe836cf42a7e4578c23dbfb8ce1f42
gaurav shinde <shinde786gaurav@gmail.com>
2019-06-14 10:29:57 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
-import './assets/css/Projectone.css'
 
-import ProjectOneMain from './Components/ProjectOne/ProjectOneMain';
-import ProjectTwoMain from './Components/ProjectTwo/ProjectTwoMain';
-import ProjectThreeMain from './Components/ProjectThree/ProjectThreeMain';
-import ProjectFourMain from './Components/ProjectFour/ProjectFourMain';
-import Header from './Components/Header';
+import Main from './Components/Main';
 
 export class App extends Component {
     render() {
         return (
             <React.Fragment>                
-                <Router>
-                  <Header/>
-                  <Route exact path="/" component={ProjectOneMain}/>
-                  <Route path="/projectone" component={ProjectOneMain}/>
-                  <Route path="/projectTwo" component={ProjectTwoMain}/>
-                  <Route path="/projectThree" component={ProjectThreeMain}/>
-                  <Route path="/projectFour" component={ProjectFourMain}/>
-                </Router>
+                <Main/>
             </React.Fragment>
         )
     }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
 export class Header extends Component {
     render() {
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/projectone">All Project</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Link to="/projectone">Project One</Link>
-                            <Link to="/projecttwo">Project Two</Link>
-                            <Link to="/projectthree">Project Three</Link>
-                            <Link to="/projectfour">Project Four</Link>
+                            <Nav.Link href="/">Project One</Nav.Link>
+                            <Nav.Link href="/projecttwo">Project Two</Nav.Link>
+                            <Nav.Link href="/projectthree">Project Three</Nav.Link>
+                            <Nav.Link href="/projectfour">Project Four</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
             </Navbar>
